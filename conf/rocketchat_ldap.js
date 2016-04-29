1f2b2e784ea0005f87922e5d6e07a5cb3c933d8e
realitygaps <realitygaps@yahoo.com>
2016-04-29 18:08:30 +0200

    Adding ldap variables

---
+++ conf/rocketchat_ldap.js
@@ -0,0 +1,41 @@
+use rocketchat
+
+
+db.rocketchat_settings.update(
+  { "_id" : "LDAP_Enable" },
+    {
+      $set: { "value": true }
+    }
+)
+db.rocketchat_settings.update(
+  { "_id" : "LDAP_Domain_Base" },
+    {
+      $set: { "value": "dc=yunohost,dc=org" }
+    }
+)
+db.rocketchat_settings.update(
+  { "_id" : "LDAP_Domain_Search_User_ID" },
+    {
+      $set: { "value": "uid" }
+    }
+)
+db.rocketchat_settings.update(
+  { "_id" : "LDAP_Domain_Search_Object_Class" },
+    {
+      $set: { "value": "inetOrgPerson" }
+    }
+)
+db.rocketchat_settings.update(
+  { "_id" : "LDAP_Sync_User_Data" },
+    {
+      $set: { "value": true }
+    }
+)
+db.rocketchat_settings.update(
+  { "_id" : "Accounts_RegistrationForm" },
+    {
+      $set: { "value": "Disabled" }
+    }
+)
+
+
