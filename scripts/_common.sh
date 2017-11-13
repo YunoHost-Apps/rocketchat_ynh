dd34eca46df450e52521e66a55a1ae035b71069f
Selamanse <selamanse@scheinfrei.info>
2017-11-13 19:01:10 +0100

    Fixes python error msg

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@ NODE_VERSION=4.7.1
 
 checkcmd() {
   curl -m 1 -s localhost:$port$path/api/v1/info | \
-    python -c "import sys, json; print json.load(sys.stdin)['success']" >/dev/null 2>&1 | \
+    python -c "import sys, json; print json.load(sys.stdin)['success']" 2>/dev/null | \
     grep "True" >/dev/null 2>&1
 }
 
