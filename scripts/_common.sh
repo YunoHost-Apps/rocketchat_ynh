28a2bf66a3e116c9a030aa1efb793e917dc7ebbd
Selamanse <selamanse@users.noreply.github.com>
2019-02-03 11:14:23 +0100

    Revert apt-key setting and restore modifications (#70)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -62,8 +62,7 @@ installdeps(){
     sudo yunohost service add mongodb -l /var/log/mongodb/mongodb.log
   else
     #Install mongodb for debian x86/x64
-    sudo apt-get install dirmngr
-    APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+    sudo apt-get install dirmngr && sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
     echo "deb http://repo.mongodb.org/apt/debian ${DEBIAN_ISSUE}/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
     sudo apt-get update
     sudo apt-get install -y mongodb-org
